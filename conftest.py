d7cb0a75cf1e92617e124ef5e3e0adbed1b36b18
sushchikh <a.sushchikh@gmail.com>
2019-10-20 13:59:15 +0300

    add comets to test and conftest

--- conftest.py
+++ conftest.py
@@ -1,8 +1,12 @@
+# импорты
 import pytest
 from selenium import webdriver
 
 
 def pytest_addoption(parser):
+    """
+    Получаем из командной строки параметр языка (fr, es, ru, и т.п.)
+    """
     parser.addoption(
         "--language", action="store", default="es", help="option: es or fr, etc..."
     )
@@ -10,11 +14,18 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="function")
 def language(request):
+    """
+    Передаем параемтр языка в тесты, чтобы они запускались с разными языками
+    """
     return request.config.getoption("--language")
 
 
 @pytest.fixture(scope="function")
 def browser():
+    """
+    Передаем параметр браузера, в данном случае Хром. Задаем допустимое ожидание элментемнов на странице (5 сек).
+    Автоматически закрываем браузер.
+    """
     print("\nСтартуем браузер")
     browser = webdriver.Chrome()
     browser.implicitly_wait(5)
--- test_items.py
+++ test_items.py
@@ -1,11 +1,18 @@
-from selenium import webdriver
+# импорты
 from time import sleep
 
 
 def test_add_btn(language, browser):
+    """
+    Тетс на наличие кнопки 'добавить в корзину' на разных языках
+    :param language: передается параметром из фикстуры (см. conftest.py)
+    :param browser: передается параметрм из фикстуры (см. conftest.py)
+    """
+    # в зависимости от языкового параметра получаем нужный url
     link = f'http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/'
     browser.get(link)
+    sleep(30)  # на мой взгляд избыточное ожидание, секунд десяти достаточно, чтобы оценить язык кнопки
+    # костыль — в переменную пушим количество элементов с классом "btn-add-to-basket", если элемент хотябы 1 - все ок
     btn_submit = len(browser.find_elements_by_class_name('btn-add-to-basket'))
-    assert btn_submit > 0, "нет кнопки 'добавить в корзину'"
-    sleep(10)
+    assert btn_submit > 0, "нет кнопки 'добавить в корзину'"  # если кнопки нет - выхлопнет ассерт
 
